9074076325765c38c76fbee36bda54849a5e6a9c
Kennedy muchunguzi <kennedymuchunguzi@gmail.com>
2025-02-12 23:27:54 +0400

    added all to a snap ready to push

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import CVMaker from './components/Cv_marker';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CVMaker />
+    </div>
+  );
+}
+
+export default App;
